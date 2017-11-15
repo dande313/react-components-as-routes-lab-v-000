748cfb9dce7cfe63ece05bf4b443932af6b65aab
Allen Anderson <dande313@gmail.com>
2017-11-15 20:05:16 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 render (
   <App />,
